941123b09a817479538afb1f999be9df9447d4e3
Omnikar <omnikar5@gmail.com>
2026-09-08 04:20:58 -0400

    New analysis system

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,4 +1,4 @@
-mod error;
+pub mod error;
 mod impls;
 
 use itertools::Itertools;
---
+++ src/analysis_new.rs
@@ -0,0 +1,293 @@
+use crate::analysis::error;
+
+use derive_more::From;
+use elsa::map::FrozenMap;
+use itertools::Itertools;
+use petgraph::visit::EdgeRef;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+use crate::generic_ir::{FunctionNode, Graph, NodeIndex};
+use crate::hir::{self, Hir};
+use crate::mir::{self, Mir, ValueInfo, types};
+use error::{ErrorKind, FancyError};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("{0}")]
+    FancyError(Box<FancyError>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, From)]
+struct HirValue {
+    node_idx: NodeIndex,
+    out_i: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, From)]
+struct MirValue {
+    node_idx: NodeIndex,
+    out_i: usize,
+}
+
+#[derive(Clone)]
+struct FunctionTranslation<'hir, 'mir> {
+    hir: &'hir Hir,
+    hir_func: &'hir hir::Function,
+    mir: &'mir RefCell<Mir>,
+    mir_graph: RefCell<Graph<mir::Node>>,
+    value_map: RefCell<HashMap<HirValue, MirValue>>,
+    info_map: FrozenMap<NodeIndex, mir::NodeMeta>,
+    span_map: RefCell<HashMap<NodeIndex, usize>>,
+}
+
+impl FunctionTranslation<'_, '_> {
+    fn add_node<const N: usize>(
+        &self,
+        node: mir::Node,
+        info: impl Into<mir::NodeMeta>,
+        inputs: impl IntoIterator<Item = MirValue>,
+    ) -> [MirValue; N] {
+        let mut graph = self.mir_graph.borrow_mut();
+        let node_idx = graph.add_node(node);
+        self.info_map.insert(node_idx, info.into());
+        for (in_i, input) in inputs.into_iter().enumerate() {
+            graph.add_edge(node_idx, input.node_idx, (input.out_i, in_i));
+        }
+        (0..N)
+            .map(|out_i| MirValue { node_idx, out_i })
+            .collect_vec()
+            .try_into()
+            .unwrap()
+    }
+
+    fn associate(&self, from: impl Into<HirValue>, to: impl Into<MirValue>) {
+        let from = from.into();
+        let to = to.into();
+        self.value_map.borrow_mut().insert(from, to);
+
+        let span = self.hir_func.spans[&from.node_idx];
+        self.span_map.borrow_mut().insert(to.node_idx, span);
+    }
+
+    fn infos<const N: usize>(&self, values: [MirValue; N]) -> [&ValueInfo; N] {
+        values.map(|val| &self.info_map[&val.node_idx][val.out_i])
+    }
+}
+
+#[derive(Clone, Copy)]
+struct AnalyzeContext<'ctx> {
+    span: &'ctx uiua::Span,
+    input_spans: &'ctx [&'ctx uiua::Span],
+    tr: &'ctx FunctionTranslation<'ctx, 'ctx>,
+}
+
+impl AnalyzeContext<'_> {
+    fn error<T>(&self, kind: ErrorKind) -> Result<T, Error> {
+        Err(Error::FancyError(Box::new(FancyError {
+            files: Rc::clone(&self.tr.hir.files),
+            span: self.span.clone(),
+            input_spans: self.input_spans.iter().map(|&x| x.clone()).collect(),
+            kind,
+        })))
+    }
+}
+
+pub fn construct_mir(hir: &hir::Hir) -> Result<mir::Mir, Error> {
+    let mut mir = RefCell::new(mir::Mir {
+        structs: Vec::new(),
+        enums: Vec::new(),
+        bindings: Vec::new(),
+        main: None,
+        spans: hir.spans.clone(),
+        files: hir.files.clone(),
+    });
+
+    if let Some((hir_main, span)) = &hir.main {
+        let main = monomorphize_and_analyze(hir_main, &[], hir, &mir)?;
+        mir.get_mut().main = Some((main, *span));
+    }
+
+    Ok(mir.into_inner())
+}
+
+fn monomorphize_and_analyze(
+    hir_func: &hir::Function,
+    inputs: impl Into<Vec<ValueInfo>>,
+    hir: &Hir,
+    mir: &RefCell<Mir>,
+) -> Result<mir::Function, Error> {
+    let inputs = inputs.into();
+
+    let mut graph = Graph::new();
+    let input_idx = graph.add_node(mir::Node::Input);
+    let mut value_map = HashMap::new();
+    for i in 0..inputs.len() {
+        value_map.insert(
+            HirValue {
+                node_idx: input_idx,
+                out_i: i,
+            },
+            MirValue {
+                node_idx: input_idx,
+                out_i: i,
+            },
+        );
+    }
+    let info_map = FrozenMap::new();
+    info_map.insert(input_idx, inputs.clone());
+
+    let translation = FunctionTranslation {
+        hir,
+        hir_func,
+        mir,
+        mir_graph: RefCell::new(graph),
+        value_map: RefCell::new(value_map),
+        info_map,
+        span_map: RefCell::new(HashMap::new()),
+    };
+
+    for node_idx in hir_func.graph.node_indices() {
+        translate_node(node_idx, &translation)?;
+    }
+
+    let graph = translation.mir_graph.into_inner();
+    let info_map = translation.info_map;
+
+    let output_idx = graph
+        .node_indices()
+        .find(|&idx| graph[idx].is_output())
+        .unwrap();
+    let outputs = graph
+        .edges(output_idx)
+        .sorted_by_key(|e| e.weight().1)
+        .map(|e| info_map[&e.target()][e.weight().0].clone())
+        .collect_vec();
+
+    Ok(mir::Function {
+        meta: mir::FunctionMeta { inputs, outputs },
+        graph,
+        input_idx,
+        output_idx,
+        node_metas: info_map.into_map(),
+        spans: translation.span_map.into_inner(),
+    })
+}
+
+fn translate_node(hir_node_idx: NodeIndex, tr: &FunctionTranslation) -> Result<(), Error> {
+    let hir_node = &tr.hir_func.graph[hir_node_idx];
+    let span = &tr.hir.spans[tr.hir_func.spans.get(&hir_node_idx).copied().unwrap_or(0)];
+
+    let (inputs, input_spans): (Vec<_>, Vec<_>) = tr
+        .hir_func
+        .graph
+        .edges(hir_node_idx)
+        .sorted_by_key(|e| e.weight().1)
+        .map(|e| {
+            (
+                tr.value_map.borrow()[&HirValue {
+                    node_idx: e.target(),
+                    out_i: e.weight().0,
+                }],
+                &tr.hir.spans[tr.hir_func.spans.get(&e.target()).copied().unwrap_or(0)],
+            )
+        })
+        .unzip();
+
+    let ctx = AnalyzeContext {
+        span,
+        input_spans: &input_spans,
+        tr,
+    };
+
+    match hir_node {
+        hir::Node::Input => {
+            // Input node is expected to already exist by this point; do nothing
+        }
+        hir::Node::Output => {
+            let [] = tr.add_node(mir::Node::Output, [], inputs);
+        }
+        hir::Node::Constant(value) => {
+            let value_info = match mir::ValueInfo::try_from(value) {
+                Ok(v) => v,
+                Err(err) => ctx.error(ErrorKind::UiuaValue(err))?,
+            };
+            let [value] = tr.add_node(mir::Node::Constant(value_info.clone()), [value_info], []);
+            tr.associate((hir_node_idx, 0), value);
+        }
+        hir::Node::FuncPrim(hir::Prim::Prim(uiua::Primitive::Not)) => {
+            let [input_info] = tr.infos([inputs[0]]);
+            let output_info = not(input_info, ctx)?;
+            let [output] = tr.add_node(
+                mir::Node::FuncPrim(mir::Prim::Prim(uiua::Primitive::Not)),
+                [output_info],
+                inputs,
+            );
+            tr.associate((hir_node_idx, 0), output);
+        }
+        _ => todo!("{hir_node:?}"),
+    }
+
+    Ok(())
+}
+
+fn pervasive_monadic(
+    input_info: &ValueInfo,
+    scalar_func: impl Fn(types::ScalarInfo) -> Result<types::ScalarInfo, Error> + Clone,
+) -> Result<ValueInfo, Error> {
+    Ok(match input_info {
+        ValueInfo::Scalar(scalar_info) => scalar_func(*scalar_info).map(ValueInfo::Scalar)?,
+        ValueInfo::Array(array_info) => match &**array_info {
+            types::ArrayInfo::Known { scalar_type, value } => {
+                // TODO: Size limit for pre-evaluation?
+                let scalar_type = pervasive_monadic(scalar_type, scalar_func.clone())?;
+                let value = types::ArrayValue {
+                    shape: value.shape.clone(),
+                    data: value
+                        .data
+                        .iter()
+                        .map(|x| pervasive_monadic(x, scalar_func.clone()))
+                        .collect::<Result<Vec<_>, _>>()?,
+                };
+                ValueInfo::Array(Box::new(types::ArrayInfo::Known { scalar_type, value }))
+            }
+            types::ArrayInfo::Ranked { scalar_type, shape } => {
+                let scalar_type = pervasive_monadic(scalar_type, scalar_func)?;
+                ValueInfo::Array(Box::new(types::ArrayInfo::Ranked {
+                    scalar_type,
+                    shape: shape.clone(),
+                }))
+            }
+            types::ArrayInfo::Unranked {
+                scalar_type,
+                shape_prefix,
+                shape_suffix,
+            } => {
+                let scalar_type = pervasive_monadic(scalar_type, scalar_func)?;
+                ValueInfo::Array(Box::new(types::ArrayInfo::Unranked {
+                    scalar_type,
+                    shape_prefix: shape_prefix.clone(),
+                    shape_suffix: shape_suffix.clone(),
+                }))
+            }
+        },
+        ValueInfo::Map(map_info) => ValueInfo::Map(Box::new(types::MapInfo {
+            key_type: map_info.key_type.clone(),
+            value_type: pervasive_monadic(&map_info.value_type, scalar_func)?,
+        })),
+        _ => todo!(),
+    })
+}
+
+fn not(input_info: &ValueInfo, ctx: AnalyzeContext) -> Result<ValueInfo, Error> {
+    use types::ScalarInfo as S;
+    pervasive_monadic(input_info, |scalar| {
+        Ok(match scalar {
+            S::Bool(b) => S::Bool(b.map(|b| !b)),
+            S::Int(i) => S::Int(i.map(|i| 1 - i)),
+            S::Float(f) => S::Float(f.map(|f| 1.0 - f)),
+            S::Char(_) => ctx.error(ErrorKind::ExpectedNumber("not"))?,
+        })
+    })
+}
